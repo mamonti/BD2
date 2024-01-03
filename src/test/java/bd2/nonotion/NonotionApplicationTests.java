596662142d637f5566ee0bb9a336be1064a8f19d
Damian Nuñez <damian.nunez.ko@gmail.com>
2024-01-03 20:47:15 -0300

    initial commit

---
+++ src/test/java/bd2/nonotion/NonotionApplicationTests.java
@@ -0,0 +1,13 @@
+package bd2.nonotion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NonotionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
