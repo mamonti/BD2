6a75648cc586cc8fe6e0193f30b211a0f8abe5ea
Ariel Aizemberg <aaizemberg@users.noreply.github.com>
2024-02-05 19:18:32 +0000

    aa

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
-FROM openjdk:21-oracle
+FROM openjdk:17-oracle
 COPY target/*.jar nonotion-service.jar
 ENTRYPOINT ["java","-jar","/nonotion-service.jar"]
--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ docker rm -f nonotion-service mongo-db
 if docker image inspect bd2-nonotion-service > /dev/null 2>&1; then
     docker image rm bd2-nonotion-service
 fi
-call .\mvnw.cmd clean package -Dmaven.test.skip=false
+mvn clean package -Dmaven.test.skip=false
 docker compose up -d
